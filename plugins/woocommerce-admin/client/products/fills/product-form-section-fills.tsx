702f26bc8142821ec12fba24cb900ccf77706251
Lourens Schep <lourensschep@gmail.com>
2023-01-25 14:06:44 +0100

    Move product section components over to product editor package

--- plugins/woocommerce-admin/client/products/fills/product-form-section-fills.tsx
+++ plugins/woocommerce-admin/client/products/fills/product-form-section-fills.tsx
@@ -2,11 +2,8 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Card, CardBody } from '@wordpress/components';
-import {
-	__experimentalWooProductSectionItem as WooProductSectionItem,
-	__experimentalProductFieldSection as ProductFieldSection,
-} from '@woocommerce/components';
+import { __experimentalWooProductSectionItem as WooProductSectionItem } from '@woocommerce/components';
+import { __experimentalProductFieldSection as ProductFieldSection } from '@woocommerce/product-editor';
 import { ProductFormSection } from '@woocommerce/data';
 
 export const Sections: React.FC< { sections: ProductFormSection[] } > = ( {
